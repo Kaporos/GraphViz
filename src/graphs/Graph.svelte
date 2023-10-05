402777e4252e1531a2fab72e06dc04d8b271e12d
maximedeboeck <119764381+maximedeboeck@users.noreply.github.com>
2023-10-05 17:52:16 +0000

    add graph generation

--- src/graphs/Graph.svelte
+++ src/graphs/Graph.svelte
@@ -1,1 +1,33 @@
-<p>Graph here</p>
+<script lang="ts">
+  
+  import { onMount } from 'svelte';
+  import { ForceGraph } from './ForceGraph'
+  import miserables_data from './miserables.json'
+  import { generateRandomLadderGraph } from './GraphGen'
+
+  let currentGraph = generateRandomLadderGraph()
+
+  window.currentGraph = currentGraph
+ 
+  let [svgNode, nodeProperties] = ForceGraph(currentGraph, {nodeGroup: d => d.group});
+
+  function newGraph() {
+    graph.removeChild(svgNode);
+    [svgNode, nodeProperties] = ForceGraph(generateRandomLadderGraph(), {nodeGroup: d => d.group});
+    graph.appendChild(svgNode);
+  }
+
+  let graph: any;
+  onMount(() => {
+    graph.appendChild(svgNode)
+  })
+
+</script>
+
+<div>
+  <div bind:this={graph}></div>
+  <button on:click={newGraph}>Run</button>
+</div>
+
+
+
